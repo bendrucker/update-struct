ace3bfdaf1af8e0829d56d33a79ab97fa0a69571
Ben Drucker <bvdrucker@gmail.com>
2015-09-22 07:48:21 -0700

    Assert that we have a struct before proceeding

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict'
 
+var assertObserv = require('assert-observ')
+var assert = require('assert-ok')
 var partial = require('ap').partial
 var forOwn = require('for-own')
 
 module.exports = updateStruct
 
 function updateStruct (struct, data) {
+  assertObserv(struct)
+  assert(struct._type === 'observ-struct', 'expected observ-struct')
+
   if (arguments.length < 2) return partial(updateStruct, struct)
   forOwn(data, update)
   return struct
